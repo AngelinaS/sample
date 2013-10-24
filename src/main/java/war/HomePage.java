db80f3c938e1ded698c8417397f91cb486ba9302
AngelinaS <angelinasorokina@inbox.lv>
2013-10-24 22:47:55 +0300

    Framework

--- src/main/java/war/HomePage.java
+++ src/main/java/war/HomePage.java
@@ -1,15 +1,52 @@
 package war;
 
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.IWrapModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+
+import java.security.PrivateKey;
+import java.util.Date;
 
 /**
  * Homepage
  */
 public class HomePage extends WebPage {
 
-	private static final long serialVersionUID = 1L;
+    private Label label;
+    private Label label2;
+    private TextField name;
+    private TextField sname;
+    public HomePage() {
+        Form form = new Form("form");
+        name = new TextField("name", new Model(""));
+        sname = new TextField("sname", new Model(""));
+        form.add(name);
+        form.add(sname);
+        form.add(new Button("button"){
+            @Override
+        public void onSubmit(){
+                String value = (String)name.getModelObject();
+                String value2 = (String)sname.getModelObject();
+                label.setModelObject(value);
+                label2.setModelObject(value2);
+                name.setModelObject("");
+                sname.setModelObject("");
+            }
+        });
+        add(form);
+        add(label = new Label ("message", new Model("")));
+        add(label2 = new Label ("message2", new Model("")));
+
+    }
 
 	// TODO Add any page properties or variables here
 
@@ -19,11 +56,11 @@ public class HomePage extends WebPage {
 	 * @param parameters
 	 *            Page parameters
 	 */
-    public HomePage(final PageParameters parameters) {
+
 
         // Add the simplest type of label
-        add(new Label("message", "If you see this message wicket is properly configured and running"));
-
         // TODO Add your page's components here
     }
-}
+
+
+
