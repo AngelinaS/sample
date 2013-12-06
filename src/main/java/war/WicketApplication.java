cc888afd5c2c6b02e494352f242c8ab595409c5f
AngelinaS <angelinasorokina@inbox.lv>
2013-12-06 13:04:07 +0200

    JPA configured with Hibernate

--- src/main/java/war/WicketApplication.java
+++ src/main/java/war/WicketApplication.java
@@ -1,26 +1,34 @@
 package war;
 
 import org.apache.wicket.protocol.http.WebApplication;
+import war.domain.Hotel;
 
-/**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
- * 
- * @see wicket.myproject.Start#main(String[])
- */
-public class WicketApplication extends WebApplication
-{    
-    /**
-     * Constructor
-     */
-	public WicketApplication()
-	{
-	}
-	
-	/**
-	 * @see wicket.Application#getHomePage()
-	 */
-	public Class getHomePage()
-	{
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class WicketApplication extends WebApplication {
+
+	public WicketApplication() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("booking");
+        EntityManager entityManager = emf.createEntityManager();
+
+        entityManager.find(Hotel.class, 1);
+
+        Hotel hotel = new Hotel();
+        hotel.setName("My Favourite Hotel :)");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(hotel);  // Save new data
+        // entityManager.merge(hotel);  // Update data
+        entityManager.close();
+
+
+        emf.close();
+    }
+
+	public Class<HomePage> getHomePage() {
 		return HomePage.class;
 	}
 
--- src/main/java/war/domain/Hotel.java
+++ src/main/java/war/domain/Hotel.java
@@ -1,6 +1,35 @@
 package war.domain;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "HOTELS")
 public class Hotel {
 
+    @Id
+    @Column(name = "HOTELID")
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    private int hotelId;
 
+    @Column(name = "NAME")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
 }
